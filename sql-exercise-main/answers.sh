e97e39f39cfa4c664785755d27dbfc78ed2870f0
Mostafa Kassem <moustafakassem12@gmail.com>
2021-03-19 21:35:29 +0200

    Basic Queries

---
+++ sql-exercise-main/answers.sh
@@ -0,0 +1,23 @@
+"SELECT Name FROM students;"
+
+"SELECT * FROM students
+WHERE Age > 30;"
+
+"SELECT * FROM students
+WHERE Age > 30
+AND Gender = 'F';"
+
+"SELECT Points FROM students
+WHERE Name = 'Alex';"
+
+"INSERT INTO students (Name, Age, Gender, Points)
+VALUES ('Mostafa', 21, 'M', 550);"
+
+"UPDATE students
+SET Points = 350
+WHERE ID = 2;"
+
+"UPDATE students
+SET Points = 180
+WHERE ID = 1;"
+
